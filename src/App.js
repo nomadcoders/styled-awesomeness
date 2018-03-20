1649e70a5c062a50fe25498b5c8b8497c6e0f2e2
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-03-20 22:33:47 +0900

    #3 injectGlobal and Extending

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import styled from "styled-components";
+import styled, { injectGlobal } from "styled-components";
+
+injectGlobal`
+  body{
+    padding:0;
+    margin:0;
+  }
+`;
 
 class App extends Component {
   render() {
@@ -7,6 +14,7 @@ class App extends Component {
       <Container>
         <Button>Hello</Button>
         <Button danger>Hello</Button>
+        <Anchor href="http://google.com">Go to google</Anchor>
       </Container>
     );
   }
@@ -31,6 +39,10 @@ const Button = styled.button`
     outline: none;
   }
   background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71")};
+`;
+
+const Anchor = Button.withComponent("a").extend`
+  text-decoration:none;
 `;
 
 export default App;
